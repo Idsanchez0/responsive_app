6c82c89ef2de9ba7dbd9a9d3fc19dabf48c22271
Idsanchez0 <33404897+Idsanchez0@users.noreply.github.com>
2024-05-06 16:52:27 -0500

    ajustes
    Ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  /// Constructor de la clase MyApp
   const MyApp({super.key});
+
+  /// Método build que retorna un widget que representa la interfaz de la aplicación
   @override
   Widget build(BuildContext context) {
+    /// LayoutBuilder permite construir widgets dependiendo de las restricciones del padre
     return LayoutBuilder(builder: (context, constraints) {
+      /// OrientationBuilder permite construir widgets dependiendo de la orientación del dispositivo
       return OrientationBuilder(builder: (context, orientation) {
+        /// Inicializa la clase SizeConfig con las restricciones y orientación actuales
         SizeConfig().init(constraints, orientation);
+
+        /// Retorna un MaterialApp que es la raíz de la aplicación
         return const MaterialApp(
-            debugShowCheckedModeBanner: false, home: Example());
+          /// Desactiva la cinta de depuración en la esquina superior derecha
+          debugShowCheckedModeBanner: false,
+
+          /// Establece el widget Example como la pantalla de inicio de la aplicación
+          home: Example(),
+        );
       });
     });
   }
